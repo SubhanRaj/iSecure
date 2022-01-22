caf4182051b8b4e4c38d9d98f6cb5bc0a0d5944c
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-22 12:57:13 +0530

    Completed Login form

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?PHP
+$login = false;
+$showError = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require 'partials/_dbconnect.php';
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $sql = "SELECT * FROM users WHERE username='$username' AND 
+    password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    $num = mysqli_num_rows($result);
+    if ($num == 1) {
+        $login = true;
+        
+    } 
+    } else {
+        $showError = "Invalid username or password";
+    }
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -14,6 +38,44 @@
 
 <body>
     <?php require 'partials/_nav.php'; ?>
+    <?PHP
+    if ($login) {
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> You are logged in. 
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    if ($showError) {
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error !</strong> ' . $showError.' 
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+
+    ?>
+    <div class="container">
+
+        <h1 class="text-center mt-3">Login to iSecure</h1>
+
+        <!-- Signup Form -->
+
+        <form action="/iSecure/login.php" method="POST">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+    
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+
+
+    </div>
 
 
     <!-- Bootstrap Bundle with Popper -->
